5c46f24555d3bdfb65c689964c5c99eb64b5a37e
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-02-26 09:19:43 +0100

    add name user and use it

---
+++ js/main.js
@@ -0,0 +1,6 @@
+alert("Bonjour, bienvenue sur mon site de Chifoumi!");
+var user = prompt("Avant de commencer à jouer, faisons les présentations... je suis Computer et toi? ");
+
+if(user){
+alert("Super " + user + " es-tu prêt à jouer ?");
+}
