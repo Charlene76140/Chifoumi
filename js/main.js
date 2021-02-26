35623a6e449f2f5d12921f8b11950f155772e48e
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-02-26 10:23:24 +0100

    add computer choice

--- js/main.js
+++ js/main.js
@@ -8,5 +8,23 @@ alert("Super " + user + " ,es-tu prêt à jouer ?");
         alert("Super utilisateur, es-tu prêt à jouer?");
     }
 
-var choice = prompt("Je te laisse choisir en premier... Pierre, Feuille ou Ciseaux?");
+var choiceUser = prompt("Je te laisse choisir en premier... Pierre, Feuille ou Ciseaux?");
 
+var choices = [
+    "Pierre",
+    "Feuille",
+    "Ciseaux",
+];
+
+var choiceComputer = Math.random();
+console.log(choiceComputer);
+
+if(choiceComputer <= 0.33){
+    console.log("Moi j'ai choisi : " + (choices[0]="Pierre"));
+}
+    else if (choiceComputer <= 0.67){
+        console.log("Moi j'ai choisi : " + (choices[1]= "Feuille"));
+    }
+else{
+    console.log("Moi j'ai choisi : " + (choices[3] = "Ciseaux"));
+}    
