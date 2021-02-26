1990bc3ef29ccc74c8ccf907490b417be551faa8
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-02-26 09:28:47 +0100

    add allow player to chose/play

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ alert("Bonjour, bienvenue sur mon site de Chifoumi!");
 var user = prompt("Avant de commencer à jouer, faisons les présentations... je suis Computer et toi? ");
 
 if(user){
-alert("Super " + user + " es-tu prêt à jouer ?");
+alert("Super " + user + " ,es-tu prêt à jouer ?");
 }
+    else{
+        alert("Super utilisateur, es-tu prêt à jouer?");
+    }
+
+var choice = prompt("Je te laisse choisir en premier... Pierre, Feuille ou Ciseaux?");
+
