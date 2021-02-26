fd229fa466abbd0b3f7b33846c2fb602dc1304e2
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-02-26 12:18:46 +0100

    add compare choice and final result

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+//Affichage bienvenue
 alert("Bonjour, bienvenue sur mon site de Chifoumi!");
+//invite user a saisir son nom
 var user = prompt("Avant de commencer à jouer, faisons les présentations... je suis Computer et toi? ");
 
+//ready to play
 if(user){
-alert("Super " + user + " ,es-tu prêt à jouer ?");
+alert("Super " + user + ", es-tu prêt à jouer ?");
 }
     else{
         alert("Super utilisateur, es-tu prêt à jouer?");
     }
 
+//choix user
 var choiceUser = prompt("Je te laisse choisir en premier... Pierre, Feuille ou Ciseaux?");
 
 var choices = [
@@ -17,14 +21,32 @@ var choices = [
 ];
 
 var choiceComputer = Math.random();
-console.log(choiceComputer);
 
 if(choiceComputer <= 0.33){
-    console.log("Moi j'ai choisi : " + (choices[0]="Pierre"));
+    choiceComputer = choices[0];
 }
     else if (choiceComputer <= 0.67){
-        console.log("Moi j'ai choisi : " + (choices[1]= "Feuille"));
+        choiceComputer = choices[1];
     }
 else{
-    console.log("Moi j'ai choisi : " + (choices[3] = "Ciseaux"));
+    choiceComputer = choices[2];
 }    
+
+alert("Moi j'ai choisi : " + choiceComputer);
+
+if(choiceUser === choiceComputer){
+    alert("Match nul!");
+}
+    else if(choiceUser === "Pierre" && choiceComputer === "Ciseaux" ||
+    choiceUser ==="Feuille" && choiceComputer === "Pierre" ||
+    choiceUser === "Ciseaux" && choiceComputer === "Feuille"){
+        alert(user + " à gagné la partie!");
+    }
+    
+else{
+    alert("Computer à gagné la partie!");
+}
+            
+
+alert("Merci d'avoir joué avec moi! A bientôt");
+
