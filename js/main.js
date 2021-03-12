b59fffc42e74d0714601e50acf480a39a9b793c0
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-03-12 11:03:52 +0100

    final version

--- js/main.js
+++ js/main.js
@@ -19,11 +19,10 @@ function getComputerChoice(){
 };
 
 
-
 //2 - Fonction permettant de récupérer le nom du joueur
 function getNameUser(){
     //recupération du nom du joueuru
-    let name = prompt("Avant de commencer à jouer, faisons les présentations... je suis Computer et toi? ");
+    let name = prompt("Faisons les présentations, je suis Computer et vous? ");
     //tant que le nom n'a pas entre 2 et 20 caractère boucle
     while(name.length < 2 || name.length > 20){
         name = prompt("Votre nom doit comporter en 2 et 20 caractères");
@@ -33,70 +32,83 @@ function getNameUser(){
 };
 
 
-
 //3 - Fonction permettant de récupérer le choix de l'utilisateur
-
 function getUserChoice(){
     do {
-        var choiceUsr = prompt("Je te laisse choisir en premier...\n" + choices.join()).toLowerCase();
+        var choiceUsr = prompt("Faites votre choix " + userName + "!\n" + choices.join()+ "?").toLowerCase();
     }
     while(!choices.includes(choiceUsr));
     return choiceUsr;
 };
 
 
-
 //4 - Fonction permettant de comparer le choix du joueur et celui du PC
 function compareResult(){
-    if(choiceUser === choiceComputer){
-        console.log("Match nul!");
-    }
+    alert("Computer a choisi : " + choiceComputer);
+        if(choiceUser === choiceComputer){
+            alert("Match nul! \n"+ userName + " : "+ pointsUser + "\nPoints Computer : " + pointsComputer);
+
+        }
         else if(choiceUser === "pierre" && choiceComputer === "ciseaux" ||
-        choiceUser ==="feuille" && choiceComputer === "pierre" ||
-        choiceUser === "ciseaux" && choiceComputer === "feuille"){
-            console.log(userName + " à gagné la manche!") 
+            choiceUser ==="feuille" && choiceComputer === "pierre" ||
+            choiceUser === "ciseaux" && choiceComputer === "feuille"){
             pointsUser += 1;
+            alert(userName + " remporte la manche \n"+ userName + " : "+ pointsUser + "\nComputer : " + pointsComputer);  
         }
     
-    else{
-        console.log("Computer à gagné la manche!");
-        pointsComputer += 1;
-    }
+        else{
+            pointsComputer += 1;
+            alert("Computer remporte la manche \n"+ userName + " : "+ pointsUser + "\nComputer : " + pointsComputer);  
+        }
 };
 
 
-//5 - Fonction rejouer
+//5 - Fonction permettant de jouer jusqu'à ce que le joueur ou le PC ai 3 victoires
+function play(){
+    while(pointsUser < 3 && pointsComputer < 3){
+        //fonction obtenir le choix du PC
+        choiceComputer = getComputerChoice();
+        //fonction obtenir le choix user
+        choiceUser = getUserChoice();
+        //fonction comparer les résultats
+        compareResult();  
+    };
+    
+    if(pointsUser === 3){
+        alert("Bravo vous avez gagné la partie!")
+    }
+    else{
+        alert("Désolé, mais c'est Computer qui a gagné la partie") 
+    }
+    //fonction rejouer
+    replay();
+};
 
 
+//6 - Fonction rejouer
+function replay(){
+    let userReplay = prompt("Vous souhaitez rejouer? \n(oui ou non)")
+    if(userReplay === "oui"){
+        pointsUser = 0;
+        pointsComputer = 0;
+       play();
+    }
+    else{
+        alert("Merci d'avoir joué et à bientôt");
+    }
+};   
 
 //---------------------------------------------- Mon code --------------------------------------------------------------------------
 
 //Message de bienvenue à l'utilisateur 
-console.log("Bonjour, bienvenue sur mon site de Chifoumi!");
+alert("Bonjour, bienvenue sur mon site de Chifoumi!");
 let userName = getNameUser();
-console.log(userName);
 
-while(pointsUser < 3 && pointsComputer < 3){
+//fonction jouer
+play();
 
-    choiceComputer = getComputerChoice();
-    console.log(choiceComputer);
 
-    choiceUser = getUserChoice();
-    console.log(choiceUser);
 
-    // console.log("Moi j'ai choisi : " + choiceComputer);
-    
-    compareResult();
-
-    console.log("Points "+ userName + " : "+ pointsUser + "\nPoints Computer : " + pointsComputer);  
-};
-
-if(pointsUser === 3){
-    console.log("Bravo vous avez gagné la partie!")
-}
-else{
-    console.log("Désolé, mais c'est Computer qui a gagné la partie")
-}
 
 
 
